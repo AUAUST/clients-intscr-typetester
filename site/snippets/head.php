1accfca98466ba1d39e059caf9bcb59423a0355a
apademide <aurian.aubert@bluewin.ch>
2022-11-29 16:41:29 +0100

    Add buttons/nav functions

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -24,14 +24,33 @@
         position: sticky;
         top: 7px;
       }
-      .controls-container {
-        margin-bottom: 7px;
+      @media (orientation: portrait) {
+        header {
+          height: 20vh;
+          overflow-y: scroll;
+        }
       }
       .button {
         display: inline-block;
         background-color: lightgray;
         border-radius: 20px;
         padding: 1px 20px;
+        margin-bottom: 7px;
+        cursor: pointer;
+      }
+      .button.disabled {
+        opacity: .5;
+        color: white;
+        pointer-events: none;
+      }
+      .button.active {
+        background-color: lime;
+        pointer-events: inherit;
+        opacity: inherit;
+        color: inherit;
+      }
+      .controls-container {
+        font-family: 'Times New Roman', Times, serif;
       }
       .text-container {
         padding: 7px 0;
@@ -40,11 +59,114 @@
       .overflow-scroll {
         white-space: nowrap;
         overflow-x: scroll;
+        overflow-y: hidden;
+        padding-bottom: .1em;
       }
       .overflow-scroll * {
         display: inline-block;
       }
     </style>
+    <script>
+      document.addEventListener('DOMContentLoaded', () => {
+
+        const nodesBtns = document.querySelectorAll('.button');
+        const fontsData = JSON.parse(document.getElementById('data-fonts').textContent);
+
+        // Path after domain
+        var urlPath = window.location.pathname;
+        // Path after first slash
+        var urlData = urlPath.substring(urlPath.indexOf('/') + 1);
+        // Font in url (before slash if any)
+        var urlFont = urlData.indexOf('/') >= 0 ?
+          urlData.substring(0, urlData.indexOf('/')) :
+          urlData;
+        // Stylesets in url (after slash)
+        var urlStylesets = urlData.indexOf('/') === -1 ? '' : urlData.substring(urlData.indexOf('/') + 1);
+        // Split string into array
+        var stylesets = urlStylesets.length >= 1 ? urlStylesets.split('/') : [];
+        // If the font exists, use it
+        if ( fontsData[urlFont] ) {
+          var font = urlFont;
+          document.body.style.fontFamily = font;
+        }
+        // Otherwise default to first one
+        else {
+          var font = Object.keys(fontsData)[0];
+        }
+        Array.from(nodesBtns).forEach(e => {
+          var type = e.dataset.type;
+          e.addEventListener('click', () => {
+            switch (type) {
+              case 'styleset':
+                var styleset = e.dataset.styleset;
+                var index = stylesets.indexOf(styleset);
+                if (index > -1) { stylesets.splice(index, 1); }
+                else { stylesets.push(styleset); };
+                
+                e.classList.toggle('active');
+                updateUrl();
+                updateBodyStyle();
+                break;
+              case 'font':
+                font = e.dataset.id
+                
+                updateUrl();
+                updateBodyStyle();
+                updateMenu({
+                  font: font,
+                  stylesets: stylesets,
+                  data: fontsData
+                });
+                window.history.pushState({},"", '/'+font+'/'+stylesets.join('/'));
+                break;
+              }
+
+          })
+        });
+
+        // Trigger updateMenu function once to init
+        updateUrl();
+        updateBodyStyle();
+        updateMenu({
+          font: font,
+          stylesets: stylesets,
+          data: fontsData
+        });
+        function updateUrl() {
+          window.history.pushState({}, "", "/");
+          window.history.pushState({},"", '/'+font+'/'+stylesets.join('/'));
+        }
+        function updateBodyStyle() {
+          var stringifiedStylesets = stylesets.length === 0 ? 'unset' : '"' + stylesets.join('", "') + '"';
+          document.body.style.fontFeatureSettings = stringifiedStylesets;
+          document.body.style.fontFamily = font;
+        }
+
+        function updateMenu({font, stylesets, data}) {
+          var availableStylesets = fontsData[font].stylesets;
+
+          Array.from(nodesBtns).forEach(e => {
+            var type = e.dataset.type;
+            e.classList.add('active')
+            // console.log('ffo')
+            
+            switch (type) {
+              case 'styleset':
+                var styleset = e.dataset.styleset;
+                availableStylesets.includes(styleset) ? e.classList.remove('disabled') : e.classList.add('disabled');
+                stylesets.includes(styleset) ? e.classList.add('active') : e.classList.remove('active')
+                break;
+              case 'font': 
+                font === e.dataset.id ? e.classList.add('active') : e.classList.remove('active');
+                break;
+            }
+          });
+        }
+
+      
+      });
+      
+    </script>
   </head>
   <body style="font-family:<?= $fonts[array_key_first($fonts)]['id'] ?>;">
     <script type="application/json" id="data-fonts"><?php echo json_encode($fonts) ?></script>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,21 +1,23 @@
 <header>
-  <div class="controls-container">
-    <?php foreach ($fonts as $font): ?>
-    
-    <div class="button" data-type="font" data-id="<?= $font['id'] ?>"><?= $font['name'] ?></div>
+  <nav>
+    <div class="controls-container">
+      <?php $index = 0; ?>
+      <?php foreach ($fonts as $font): ?>
+      <div class="button <?php $index++; echo $index < 2 ? 'active' : '';?>" data-type="font" data-id="<?= $font['id'] ?>"><?= $font['name'] ?></div>
 
-    <?php endforeach ?>
-  </div>
-  <div class="controls-container">
-    <div class="button" data-type="toggle" data-toggle="ss01">ss01</div>
-    <div class="button" data-type="toggle" data-toggle="ss02">ss02</div>
-    <div class="button" data-type="toggle" data-toggle="ss03">ss03</div>
-    <div class="button" data-type="toggle" data-toggle="ss04">ss04</div>
-    <div class="button" data-type="toggle" data-toggle="ss05">ss05</div>
-    <div class="button" data-type="toggle" data-toggle="ss06">ss06</div>
-    <div class="button" data-type="toggle" data-toggle="ss07">ss07</div>
-    <div class="button" data-type="toggle" data-toggle="ss08">ss08</div>
-    <div class="button" data-type="toggle" data-toggle="ss09">ss09</div>
-    <div class="button" data-type="toggle" data-toggle="ss10">ss10</div>
-  </div>
+      <?php endforeach ?>
+    </div>
+    <div class="controls-container">
+      <div class="button" data-type="styleset" data-styleset="ss01">ss01</div>
+      <div class="button" data-type="styleset" data-styleset="ss02">ss02</div>
+      <div class="button" data-type="styleset" data-styleset="ss03">ss03</div>
+      <div class="button" data-type="styleset" data-styleset="ss04">ss04</div>
+      <div class="button" data-type="styleset" data-styleset="ss05">ss05</div>
+      <div class="button" data-type="styleset" data-styleset="ss06">ss06</div>
+      <div class="button" data-type="styleset" data-styleset="ss07">ss07</div>
+      <div class="button" data-type="styleset" data-styleset="ss08">ss08</div>
+      <div class="button" data-type="styleset" data-styleset="ss09">ss09</div>
+      <div class="button" data-type="styleset" data-styleset="ss10">ss10</div>
+    </div>
+  </nav>
 </header>
