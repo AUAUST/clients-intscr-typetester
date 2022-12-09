ff5a9648af78cf57b472912195b40c198857fd44
apademide <aurian.aubert@bluewin.ch>
2022-12-09 22:53:51 +0100

    Move current setup to archive

---
+++ archive/assets/js/input.js
@@ -0,0 +1,15 @@
+function previewFile() {
+  const preview = document.querySelector('input');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    // convert image file to base64 string
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+
+  }
+}
--- archive/site/templates/default.php
+++ archive/site/templates/default.php
@@ -1,4 +1,6 @@
 <?= snippet('head') ?>
 <?= snippet('header') ?>
 <?= snippet('textcontent/main') ?>
+<input type="file" onchange="previewFile()">
+<?= js('assets/js/input.js')?>
 <?= snippet('footer') ?>
