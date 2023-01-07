766404c3136b38e1bc76f4bd739e628e6c083766
apademide <contact@apademide.ch>
2023-01-07 21:24:25 +0100

    Remove Vue router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
     <h1>Hello App!</h1>
-    <p>
-      <router-link :to="{ name: 'home' }">Go to Home</router-link>
-      <router-link :to="{ name: 'about' }">Go to About</router-link>
-    </p>
-    <router-view></router-view>
   </div>
 </template>
 
-<script setup lang="ts">
-import RouterVue from "vue-router";
-</script>
+<script setup lang="ts"></script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import "@/style.css";
 import App from "@/App.vue";
-import router from "@/plugins/router.js";
 
-createApp(App).use(router).mount("#app");
+createApp(App).mount("#app");
