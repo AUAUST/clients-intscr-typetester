06ce40fc44d694f776a402cbd64c1cec11a8821e
apademide <contact@apademide.ch>
2023-01-15 13:53:05 +0100

    Add useLocalStorage

--- src/components/content/Paragraph.vue
+++ src/components/content/Paragraph.vue
@@ -2,9 +2,11 @@
   height: {{ windowData.size.height }}<br />
   width: {{ windowData.size.width }}<br />
   scale: {{ windowData.size.currentScale }}<br />
-  brightness: {{ windowData.brightness.className }}
+  brightness: {{ windowData.brightness.className }}<br />
+  localStorage: {{ localStorageData.get("uwu") }}
 </template>
 
 <script setup lang="ts">
 import { windowData } from "~/composables/useWindow";
+import { localStorageData } from "~/composables/useLocalStorage";
 </script>
---
+++ src/composables/useLocalStorage.ts
@@ -0,0 +1,54 @@
+import { reactive } from "vue";
+
+class localStorageClass {
+  storage: { [k: string]: any };
+  constructor() {
+    this.storage = reactive({});
+  }
+  get(key: string): { exists: boolean; value: unknown } {
+    if (key) {
+      // try to get from the cached/reactive storage
+      let value: unknown = this.storage[key];
+      // if it's undefined, try to get from the actual local storage
+      if (value === undefined) {
+        let JSONvalue = window.localStorage.getItem(key);
+        // JSONvalue is either a JSON string or null
+        if (typeof JSONvalue === "string") {
+          value = JSON.parse(JSONvalue);
+          this.storage[key] = value;
+          return {
+            exists: true,
+            value: value,
+          };
+        }
+        // if it's null, the value doesn't exist
+        return {
+          exists: false,
+          value: null,
+        };
+      }
+      // if the value is defined, return it from cache
+      return {
+        exists: true,
+        value: value,
+      };
+    }
+    return {
+      exists: false,
+      value: null,
+    };
+  }
+  set(key: string, value: unknown) {
+    if (key) {
+      this.storage[key] = value;
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      console.error(
+        `Tried to set a localStorage value with an invalid key: ${key}`
+      );
+    }
+    return this;
+  }
+}
+
+export const localStorageData = new localStorageClass();
