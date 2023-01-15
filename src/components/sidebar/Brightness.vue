ee5197ec926128e85043a660ef4e50333b2f0dd0
apademide <contact@apademide.ch>
2023-01-15 11:57:56 +0100

    Show selected brightness settings

--- src/components/sidebar/Brightness.vue
+++ src/components/sidebar/Brightness.vue
@@ -4,18 +4,27 @@
       @click="windowData.setBrightness('theme-light')"
       type="fit-width"
       title="A lighter interface."
+      :active="
+        !!windowData.brightness.userSelected &&
+        windowData.brightness.className === 'theme-light'
+      "
       >Light</Button
     >
     <Button
       @click="windowData.setBrightness(null)"
       type="fit-width"
       title="Use the user's settings to determine wether to use dark or light mode."
+      :active="windowData.brightness.userSelected === null"
       >Default</Button
     >
     <Button
       @click="windowData.setBrightness('theme-dark')"
       type="fit-width"
       title="A darker interface."
+      :active="
+        !!windowData.brightness.userSelected &&
+        windowData.brightness.className === 'theme-dark'
+      "
       >Dark</Button
     >
   </div>
