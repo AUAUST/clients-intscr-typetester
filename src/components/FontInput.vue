b0cb3eb2aeb3000e3b22ebf50883a74ce42b8350
apademide <contact@apademide.ch>
2023-03-05 18:50:01 +0100

    Nest #font-input in a container and add overlay

--- src/components/FontInput.vue
+++ src/components/FontInput.vue
@@ -1,22 +1,27 @@
 <template>
-  <input
-    id="font-input"
-    type="file"
-    accept="*.ttf, *.otf, *.woff, *.woff2"
-    ref="fontInput"
-    @change="(event) => {
+  <font-input-container>
+    <input
+      id="font-input"
+      type="file"
+      accept="*.ttf, *.otf, *.woff, *.woff2"
+      ref="fontInput"
+      @change="(event) => {
       fonts.fontInputChanged({
         file: (event.target as HTMLInputElement).files![0],
       })}
-    "
-    hidden
-  />
+      "
+      hidden
+    />
+    <font-input-overlay :visible="viewport.dropZoneVisible.value">
+    </font-input-overlay>
+  </font-input-container>
 </template>
 
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
 
 import { fonts } from "~/composables/useFont";
+import { viewport } from "~/composables/useViewport";
 
 const fontInput = ref<HTMLInputElement>();
 
@@ -30,4 +35,19 @@ onUnmounted(() => {
 
 <style lang="scss">
 @use "@/assets/style/variables" as v;
+
+font-input-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  opacity: 0.5;
+  z-index: 1;
+
+  &[visible="true"] {
+    background-color: red;
+  }
+}
 </style>
