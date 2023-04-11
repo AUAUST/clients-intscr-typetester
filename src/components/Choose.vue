01b7872edc2c5cd56fe357c6d408e0dfbec71828
apademide <contact@apademide.ch>
2023-04-11 22:14:54 +0200

    imp: add Select a fil button

--- src/components/Choose.vue
+++ src/components/Choose.vue
@@ -1,3 +1,9 @@
 <template>
-  <view-item style="--width: 100%"> choose font </view-item>
+  <view-item style="--width: 100%">
+    <Button @click="fonts.openFileDialog()"> Select a file </Button>
+  </view-item>
 </template>
+
+<script setup lang="ts">
+import { fonts } from "~/composables/useFont";
+</script>
