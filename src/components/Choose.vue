1e2563a9b6e7b543d893d01eb9c20c5b7ae6c13f
apademide <contact@apademide.ch>
2023-04-21 18:56:48 +0200

    css: basic styling for choose view

--- src/components/Choose.vue
+++ src/components/Choose.vue
@@ -1,16 +1,28 @@
 <template>
   <view-item style="--width: 100%">
-    <div>
+    <no-font-container>
       <div>
         Drop or
-        <Button @click="fonts.openFileDialog()"> Select </Button>
+        <Button @click="fonts.openFileDialog()"> Choose </Button>
         a file to start testing your font.
       </div>
       <div>You may also try an Interscript font</div>
-    </div>
+    </no-font-container>
   </view-item>
 </template>
 
 <script setup lang="ts">
 import { fonts } from "~/composables/useFont";
+import Button from "@/components/ui/Button.vue";
 </script>
+
+<style lang="scss">
+no-font-container {
+  display: block;
+  margin-block: auto;
+  justify-self: center;
+  align-self: center;
+
+  text-align: center;
+}
+</style>
