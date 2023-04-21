4e9eb93309b9449fabd3481987c3b5b59c8531ac
apademide <contact@apademide.ch>
2023-04-22 00:12:11 +0200

    snap: try messing with font metrics

--- src/components/content/Paragraph.vue
+++ src/components/content/Paragraph.vue
@@ -1,7 +1,17 @@
 <template>
-  <div v-if="font">
-    {{ CHAR }}:
-    <hr />
+  <div
+    v-if="font"
+    class="container"
+    :style="{
+      '--unitsPerEm': font.unitsPerEm,
+    }"
+  >
+    <div class="char">
+      {{ CHAR }}
+    </div>
+    <div class="ascent" :style="{ '--height': font.ascent }"></div>
+    <div class="descent" :style="{ '--height': font.descent }"></div>
+    <div class="lineGap" :style="{ '--height': font.lineGap }"></div>
   </div>
 </template>
 
@@ -11,13 +21,42 @@ import { fonts } from "~/composables/useFont";
 const font = fonts.currentFont.value;
 
 // CHAR = fi ligature
-const CHAR = "ﬁ";
-// const CHAR = "a";
+// const CHAR = "ﬁ";
+const CHAR = "k";
 const UNI = CHAR.charCodeAt(0);
+const GLYPH = font?.layout(CHAR).glyphs[0];
 
 console.log(CHAR, UNI);
 console.log(font?.glyphForCodePoint(UNI));
 console.log(font?.hasGlyphForCodePoint(UNI));
 console.log(font?.glyphsForString(CHAR));
-console.log(font?.layout(CHAR));
 </script>
+
+<style lang="scss">
+.container {
+  font-size: 100px;
+  position: relative;
+  --unit: 1em / var(--unitsPerEm);
+  .char {
+    font-size: 1em;
+  }
+  .ascent {
+    position: absolute;
+    bottom: calc(var(--height) * var(--unit));
+    border-top: 1px solid red;
+    width: 100%;
+  }
+  .descent {
+    position: absolute;
+    bottom: calc(var(--height) * var(--unit));
+    border-bottom: 1px solid orange;
+    width: 100%;
+  }
+  .lineGap {
+    position: absolute;
+    bottom: calc(var(--height) * var(--unit));
+    border-bottom: 1px solid yellow;
+    width: 100%;
+  }
+}
+</style>
