f578c86b0fa5c16053a67a0def74df2b28b190dd
apademide <contact@apademide.ch>
2023-04-24 18:00:59 +0200

    feat: font features available in sidebar

--- src/components/sidebar/FontSettings.vue
+++ src/components/sidebar/FontSettings.vue
@@ -1,18 +1,35 @@
 <template>
-  <ButtonsGroup title="Theme">
-    <Button
-      @click="_console.log('Light')"
-      size="fit-width"
-      title="Hello"
-      :active="false"
-      >Light</Button
-    >
-  </ButtonsGroup>
+  <Button
+    v-for="feature in fontFeatures"
+    @click="
+      if (enabledFeatures.has(feature)) {
+        enabledFeatures.delete(feature);
+      } else {
+        enabledFeatures.add(feature);
+      }
+    "
+    size="fit-width"
+    :title="feature"
+    :active="enabledFeatures.has(feature)"
+  >
+    {{ feature }}
+  </Button>
+  <div>
+    {{ enabledFeatures }}
+  </div>
 </template>
 
 <script setup lang="ts">
 import Button from "@/components/ui/Button.vue";
 import ButtonsGroup from "@/components/ui/ButtonsGroup.vue";
+import { computed, reactive } from "vue";
 
-const _console = console;
+import { fonts } from "~/composables/useFont";
+
+const font = computed(() => fonts.currentFont.value);
+const fontFeatures = computed(() =>
+  font.value ? font.value.availableFeatures : []
+);
+
+const enabledFeatures = reactive(new Set<string>());
 </script>
