56ecdec17fa26a1b3a4769c79ba9636f6c2464b2
apademide <contact@apademide.ch>
2023-04-28 11:05:31 +0200

    config: sign code

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import "@/assets/style/index.scss";
 import App from "@/App.vue";
 
+console.log(
+  "%cAUAUST.CH DESIGN AUTOMATISÉ",
+  "color:white;background-color:#170049;font-family:monospace;font-size:13px;padding:0px 1.5ch 1px 1.5ch;"
+);
+
 createApp(App).mount("#app");
