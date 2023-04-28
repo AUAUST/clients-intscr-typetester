7d2f58cb50f9853c92e1a151cd1416ce1b191585
apademide <contact@apademide.ch>
2023-04-28 15:30:06 +0200

    style: organize code

--- src/composables/useFonts.ts
+++ src/composables/useFonts.ts
@@ -6,19 +6,26 @@ import { computed, ref } from "vue";
 // function()s become actions
 
 export const useFonts = defineStore("fonts", () => {
+  // ================================================
+  // States
   const fonts = {};
   const count = ref(0);
 
+  // ================================================
+  // Getters
+  const fontsCount = computed(() => {
+    return Object.keys(fonts).length;
+  });
+
+  // ================================================
+  // Actions
   function increment() {
     console.log("increment");
     console.log(count.value);
     count.value++;
   }
 
-  const fontsCount = computed(() => {
-    return Object.keys(fonts).length;
-  });
-
+  // ================================================
   return {
     fonts,
     count,
